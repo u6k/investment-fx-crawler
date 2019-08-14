256a309226f74afb87bd1fa47cc3e509c93a5d88
u6k <u6k.apps@gmail.com>
2019-08-14 21:50:12 +0900

    Added: parameter, logging

--- histdata_scrapy/spiders/histdata.py
+++ histdata_scrapy/spiders/histdata.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from ..app_logging import get_app_logger
+
 
 class HistDataSpider(scrapy.Spider):
     name = 'histdata'
@@ -8,16 +10,20 @@ class HistDataSpider(scrapy.Spider):
     start_urls = ['http://www.histdata.com/download-free-forex-data/?/ascii/tick-data-quotes']
 
     target_fxpairs = ["USD/JPY", "EUR/USD"]
-    target_month = (2019, 7)
+    # target_month = (2019, 8)
+    target_month = None
+
+    L = get_app_logger()
 
     def parse(self, response):
+        self.L.info(f"parse: url={response.url}")
+
         for link_cell in response.xpath("//div[@id='content']/div/table/tr/td"):
             url = link_cell.xpath("a/@href").extract()[0]
             url = response.urljoin(url)
             fxpair = link_cell.xpath("a/strong/text()").extract()[0]
 
-            print(url)
-            print(fxpair)
+            self.L.info(f"fxpair={fxpair}, url={url}")
 
             if fxpair not in self.target_fxpairs:
                 continue
@@ -25,13 +31,14 @@ class HistDataSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse_years)
 
     def parse_years(self, response):
+        self.L.info(f"parse_years: url={response.url}")
+
         for link_cell in response.xpath("//div[@class='page-content']/table/tr/td"):
             url = link_cell.xpath("a/@href").extract()[0]
             url = response.urljoin(url)
             year = int(link_cell.xpath("a/strong/text()").extract()[0])
 
-            print(url)
-            print(year)
+            self.L.info(f"year={year}, url={url}")
 
             if self.target_month is not None:
                 if self.target_month[0] != year:
@@ -40,15 +47,15 @@ class HistDataSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse_months)
 
     def parse_months(self, response):
+        self.L.info(f"parse_months: url={response.url}")
+
         for link in response.xpath("//div[@class='page-content']/p/a"):
             url = link.xpath("@href").extract()[0]
             url = response.urljoin(url)
             year = int(url.split("/")[-2])
             month = int(url.split("/")[-1])
 
-            print(url)
-            print(year)
-            print(month)
+            self.L.info(f"year={year}, month={month}, url={url}")
 
             if self.target_month is not None:
                 if self.target_month[0] != year or self.target_month[1] != month:
@@ -57,4 +64,4 @@ class HistDataSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse_download)
 
     def parse_download(self, response):
-        print(response.url)
+        self.L.info(f"parse_download: url={response.url}")
