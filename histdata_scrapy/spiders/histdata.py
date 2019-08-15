d4c4a3e623e9dee2107354ea267516f419060af5
u6k <u6k.apps@gmail.com>
2019-08-16 01:54:57 +0900

    Changed: use scrapy logger

--- histdata_scrapy/spiders/histdata.py
+++ histdata_scrapy/spiders/histdata.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-from ..app_logging import get_app_logger
-
 
 class HistDataSpider(scrapy.Spider):
     name = 'histdata'
@@ -13,17 +11,15 @@ class HistDataSpider(scrapy.Spider):
     # target_month = (2019, 8)
     target_month = None
 
-    L = get_app_logger()
-
     def parse(self, response):
-        self.L.info(f"parse: url={response.url}")
+        self.logger.info(f"parse: url={response.url}")
 
         for link_cell in response.xpath("//div[@id='content']/div/table/tr/td"):
             url = link_cell.xpath("a/@href").extract()[0]
             url = response.urljoin(url)
             fxpair = link_cell.xpath("a/strong/text()").extract()[0]
 
-            self.L.info(f"fxpair={fxpair}, url={url}")
+            self.logger.info(f"fxpair={fxpair}, url={url}")
 
             if fxpair not in self.target_fxpairs:
                 continue
@@ -31,14 +27,14 @@ class HistDataSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse_years)
 
     def parse_years(self, response):
-        self.L.info(f"parse_years: url={response.url}")
+        self.logger.info(f"parse_years: url={response.url}")
 
         for link_cell in response.xpath("//div[@class='page-content']/table/tr/td"):
             url = link_cell.xpath("a/@href").extract()[0]
             url = response.urljoin(url)
             year = int(link_cell.xpath("a/strong/text()").extract()[0])
 
-            self.L.info(f"year={year}, url={url}")
+            self.logger.info(f"year={year}, url={url}")
 
             if self.target_month is not None:
                 if self.target_month[0] != year:
@@ -47,7 +43,7 @@ class HistDataSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse_months)
 
     def parse_months(self, response):
-        self.L.info(f"parse_months: url={response.url}")
+        self.logger.info(f"parse_months: url={response.url}")
 
         for link in response.xpath("//div[@class='page-content']/p/a"):
             url = link.xpath("@href").extract()[0]
@@ -55,7 +51,7 @@ class HistDataSpider(scrapy.Spider):
             year = int(url.split("/")[-2])
             month = int(url.split("/")[-1])
 
-            self.L.info(f"year={year}, month={month}, url={url}")
+            self.logger.info(f"year={year}, month={month}, url={url}")
 
             if self.target_month is not None:
                 if self.target_month[0] != year or self.target_month[1] != month:
@@ -64,4 +60,24 @@ class HistDataSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse_download)
 
     def parse_download(self, response):
-        self.L.info(f"parse_download: url={response.url}")
+        self.logger.info(f"parse_download: url={response.url}")
+
+        for form_node in response.xpath("//form[@id='file_down']"):
+            url = form_node.xpath("@action").extract()[0]
+            url = response.urljoin(url)
+
+            params = {
+                "tk": form_node.xpath("input[@id='tk']/@value").extract()[0],
+                "date": form_node.xpath("input[@id='date']/@value").extract()[0],
+                "datemonth": form_node.xpath("input[@id='datemonth']/@value").extract()[0],
+                "platform": form_node.xpath("input[@id='platform']/@value").extract()[0],
+                "timeframe": form_node.xpath("input[@id='timeframe']/@value").extract()[0],
+                "fxpair": form_node.xpath("input[@id='fxpair']/@value").extract()[0]
+            }
+
+            self.logger.info(f"url={url}, params={params}")
+
+            yield scrapy.FormRequest(url, formdata=params, callback=self.parse_zip_download)
+
+    def parse_zip_download(self, response):
+        self.logger.info(f"parse_zip_download: content-type={response.headers['Content-Type']}, size={len(response.body)}")
