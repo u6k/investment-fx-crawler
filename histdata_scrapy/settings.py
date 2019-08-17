f386078d699e93a9a2d8a7323bd66e4a6af80fad
u6k <u6k.apps@gmail.com>
2019-08-18 00:23:48 +0900

    Changed: cache expire 0 -> 86400 (1 day)

--- histdata_scrapy/settings.py
+++ histdata_scrapy/settings.py
@@ -86,7 +86,7 @@ DOWNLOAD_DELAY = 1
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_EXPIRATION_SECS = 86400
 HTTPCACHE_DIR = 'httpcache'
 HTTPCACHE_IGNORE_HTTP_CODES = []
 HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
